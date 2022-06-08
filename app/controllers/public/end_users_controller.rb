79ac2841a2249235fc4c7e4952b8ffe45fc9a4ba
napeblossoms <napeblossoms@gmail.com>
2022-06-08 09:30:51 +0000

    [Add]プロフィール編集機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,13 +1,24 @@
 class Public::EndUsersController < ApplicationController
   def show
     @end_user = EndUser.find(params[:id])
+    @post_sushis = @end_user.post_sushis
+
   end
-  
+
   def edit
+    @end_user = EndUser.find(params[:id])
   end
-  
+
   def update
+    @end_user = EndUser.find(params[:id])
+    @end_user.update(end_user_params)
+    redirect_to public_end_user_path(@end_user.id)
   end
-  
-  
+
+    private
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction, :profile_image)
+  end
+
+
 end
