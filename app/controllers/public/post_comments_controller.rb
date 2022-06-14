013e6dcf8c282dda12fd2657e9da06363c68a96a
napeblossoms <napeblossoms@gmail.com>
2022-06-14 15:45:01 +0000

    [Update]コメント非同期

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class Public::PostCommentsController < ApplicationController
   def create
-    post_sushi = PostSushi.find(params[:post_sushi_id])
+    @post_sushi = PostSushi.find(params[:post_sushi_id])
     comment = PostComment.new(post_comment_params)
     comment.end_user_id = current_end_user.id
-    comment.post_sushi_id = post_sushi.id
+    comment.post_sushi_id = @post_sushi.id
     comment.save
-    redirect_to  public_post_sushi_path(post_sushi)
+    # redirect_to  public_post_sushi_path(post_sushi)
   end
 
   def destroy
+    @post_sushi = PostSushi.find(params[:post_sushi_id])
     PostComment.find(params[:id]).destroy
-    redirect_to public_post_sushi_path(params[:post_sushi_id])
+    # redirect_to public_post_sushi_path(params[:post_sushi_id])
   end
 
   private
