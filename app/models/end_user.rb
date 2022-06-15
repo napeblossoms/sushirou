a8e2718f143d9b4cf09f26fbc8d2bbb8cab993cd
napeblossoms <napeblossoms@gmail.com>
2022-06-15 16:52:36 +0000

    [Update]レイアウト修正

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -30,12 +30,12 @@ class EndUser < ApplicationRecord
   end
 
 
-  def get_profile_image(width,height)
+  def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image
   end
 
   def self.guest #ゲスト
