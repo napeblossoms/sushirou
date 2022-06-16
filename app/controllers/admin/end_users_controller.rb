4565695dc516106ea804e9219e4224e3d5329cfd
napeblossoms <napeblossoms@gmail.com>
2022-06-16 13:49:59 +0000

    [Add]管理者退会機能

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -10,9 +10,19 @@ class Admin::EndUsersController < ApplicationController
     end
     
     def edit
+      @end_user = EndUser.find(params[:id])
     end
     
     def update
+      @end_user = EndUser.find(params[:id])
+      @end_user.update(end_user_params)
+      redirect_to  admin_end_user_path(@end_user)
+    end
+    
+    private
+    
+    def end_user_params
+      params.require(:end_user).permit(:name, :email, :is_deleted)
     end
     
 end
