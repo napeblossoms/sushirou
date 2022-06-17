63974760ff8ef4f19515b0c83110d97c0ab6a99b
napeblossoms <napeblossoms@gmail.com>
2022-06-17 07:27:20 +0000

    [Update]レイアウト修正、部分テンプレ

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#top" 
+  get '/about' => 'homes#about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :public do
    resources :post_sushis, only: [:new, :create, :index, :edit, :show, :update, :destroy] do
