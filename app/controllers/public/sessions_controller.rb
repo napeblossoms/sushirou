a319619839685f9b7d6e641c80b1550f26a16ee9
napeblossoms <napeblossoms@gmail.com>
2022-06-18 09:21:56 +0000

    fixed deploy

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,38 +1,14 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
-  before action :end_useer_state, only: [:create]
-  
- protected
+  before action :end_user_state, only: [:create]
+
+  protected
   #退会してるか確認
-  def end_user_status
-    @enduser = EndUser.find_by(name: params[:end_user][:email])
+  def end_user_state
+    @end_user = EndUser.find_by(name: params[:end_user][:email])
     if @end_user
-      if @user.valid_password?(params[:end_user][:password]) && (@end_user.is_delete == true)
-        redirect_to new_user_session_path
+      if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.is_delete == true)
+        redirect_to new_end_user_session_path
       end
     end
   end
 end
-
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
