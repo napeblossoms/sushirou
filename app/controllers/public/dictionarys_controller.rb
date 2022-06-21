be4ffd4e149a84a6698db9a2abda86a6e0dc1279
napeblossoms <napeblossoms@gmail.com>
2022-06-21 12:49:01 +0000

    [Update]dictionary

--- app/controllers/public/dictionarys_controller.rb
+++ app/controllers/public/dictionarys_controller.rb
@@ -12,7 +12,10 @@ class Public::DictionarysController < ApplicationController
   end
 
   def index
-    @dictionarys = Dictionary.all
+    # @dictionarys = Dictionary.all
+    @dictionary = Dictionary.new
+    method = params[:search_method]#haru,natumaki,huyu
+    @dictionarys = Dictionary.search(method)
   end
 
   def show
@@ -34,6 +37,12 @@ class Public::DictionarysController < ApplicationController
     @dictionary.destroy
     redirect_to public_dictionarys_path
   end
+
+  # def search
+  #   method = params[:search_method]
+  #   word = params[:search_word]
+  #   @dictionarys = Dictionary.search(method,word)
+  # end
 
   private
 
--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,2 +1,17 @@
 class Dictionary < ApplicationRecord
+    def self.search(method)
+                if method == "全部"
+                   @dictionarys = Dictionary.all
+                elsif method == "春"
+                   @dictionarys = Dictionary.where(season: "#{method}")
+                elsif method == "夏"
+                   @dictionarys = Dictionary.where(season: "#{method}")
+                elsif method == "秋"
+                   @dictionarys = Dictionary.where(season: "#{method}")
+                elsif method == "冬"
+                   @dictionarys = Dictionary.where(season: "#{method}")
+                else
+                  @dictionarys = Dictionary.all
+                end
+    end
 end
