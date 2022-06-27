f5cf70e1c718abda88d3b0124833c8591a585418
napeblossoms <napeblossoms@gmail.com>
2022-06-27 03:00:24 +0000

    [Add]スシ辞典、フラッシュメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
+  add_flash_types :success, :info, :warning, :danger
+
   def after_sign_in_path_for(resource_or_scope)
     if resource.is_a?(Admin)
       admin_post_sushis_path
@@ -8,7 +9,7 @@ class ApplicationController < ActionController::Base
        public_post_sushis_path
     end
   end
-  
+
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       root_path
@@ -16,7 +17,7 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
- 
+
   protected
 
   def configure_permitted_parameters
